718f60c1c30f656b37386b711742945b9fe98f1d
Konstantin Gorodetski <kgorodetsky@yandex.ru>
2017-04-22 21:00:09 +0300

    конверсия из рад в дек

---
+++ core/src/by/ld38/game/system/logic/RadMovementSystem.java
@@ -0,0 +1,30 @@
+package by.ld38.game.system.logic;
+
+import by.ld38.game.component.base.RadPosition;
+import by.ld38.game.component.base.Velocity;
+import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
+import com.artemis.annotations.Wire;
+import com.artemis.systems.IteratingSystem;
+
+@Wire
+public class RadMovementSystem extends IteratingSystem {
+
+    private ComponentMapper<Velocity> vm;
+    private ComponentMapper<RadPosition> rpm;
+
+    /**
+     * Creates a new EntityProcessingSystem.
+     */
+    public RadMovementSystem() {
+        super(Aspect.all(RadPosition.class, Velocity.class));
+    }
+
+    @Override
+    protected void process(int entityId) {
+        Velocity v = vm.get(entityId);
+        RadPosition rp = rpm.get(entityId);
+        rp.radX += v.getRadVX();
+        rp.radY += v.getRadVY();
+    }
+}
