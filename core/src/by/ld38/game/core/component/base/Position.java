17786b026a0abc599ef541bbae85f8ba7ae62c36
shk <shark729@mail.ru>
2017-04-23 21:11:43 +0300

    Add basic renderers;

---
+++ core/src/by/ld38/game/core/component/base/Position.java
@@ -0,0 +1,13 @@
+package by.ld38.game.core.component.base;
+
+import com.artemis.Component;
+
+/**
+ * @author SharK729
+ * @since 1.0
+ */
+public class Position extends Component {
+    public float x;
+    public float y;
+    public float z = 100f; // Rendering order
+}
